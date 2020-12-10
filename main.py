77107a3cef331dd2fcbfdbcabbdf027451539132
Maurice Mertens <maurice.mertens95@googlemail.com>
2020-12-10 11:55:59 +0100

    Fixed typo

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ client = discord.Client()
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
 
+
 @client.event
 async def on_message(message):
-    if message.autho == client.user:
+    if message.author == client.user:
         return
-    
-    if message.content.startswith('!test'):
-        await message.channel.send('Passed!')
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
 client.run(os.getenv("DISCORD_TOKEN"))
